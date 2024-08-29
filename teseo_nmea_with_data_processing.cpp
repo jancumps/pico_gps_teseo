b5d2a632cb94417dd8144135e5595b4019ea3701
Jan Cumps <jcumps@gmail.com>
2024-08-29 20:07:52 +0200

    naming conventions of µOS

--- teseo_nmea_with_data_processing.cpp
+++ teseo_nmea_with_data_processing.cpp
@@ -92,9 +92,9 @@ size_t count_constellations(const nmea::nmea::talker_id source) {
 int main() {
     init();
 
-    gps.getWriteCallback().set([](const std::string& s) -> void { write(s); });
-    gps.getReadCallback().set([](std::string& s) -> void { read(s); });
-    gps.getResetCallback().set([]() -> void { reset(); });
+    gps.writer().set([](const std::string& s) -> void { write(s); });
+    gps.reader().set([](std::string& s) -> void { read(s); });
+    gps.resetter().set([]() -> void { reset(); });
 
     /*
     when the teseo is preset for i2c according to AN5203,
