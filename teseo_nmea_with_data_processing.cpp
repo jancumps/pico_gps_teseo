5f21e8c7e80be67275bb89c5fff659d637e8d5eb
Jan Cumps <jcumps@gmail.com>
2025-12-23 10:58:26 +0100

    gcc15 released for both risc and arm. Ready for release

--- teseo_nmea_with_data_processing.cpp
+++ teseo_nmea_with_data_processing.cpp
@@ -61,7 +61,7 @@ bool retrieve_rmc() {
     bool valid; // intentionally uninitialised
     valid = gps.ask_rmc(reply);
     if (!valid) { return false; }
-    if (auto&& [result] = nmea::rmc::from_data(reply)) { // TODO: when GCC 15.1 released for ARM, use auto [result]
+    if (auto&& [result] = nmea::rmc::from_data(reply)) {
         valid = true;
         rmc = result;
     }
